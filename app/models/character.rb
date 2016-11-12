bac5f7aefc11209c1b4e9995dcf2bcd4b3041d5b
Adam <alk5198@gmail.com>
2016-11-12 16:35:30 -0600

    Added comments
    added comments to events

---
+++ app/models/character.rb
@@ -0,0 +1,23 @@
+class Character < ActiveRecord::Base
+
+  belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
+  
+
+  # Character:
+  #  - movie_id: must be present
+
+  validates :movie_id, :presence => true
+
+  #  - actor_id: must be present
+  validates :actor_id, :presence => true
+
+  #  - name: no rules
+  #
+  # Actor:
+  #  - name: must be present; must be unique in combination with dob
+validates :name, :presence => true, :uniqueness => { :scope => :dob}
+  #  - dob: no rules
+  #  - bio: no rules
+  #  - image_url: no rules
+
+end
